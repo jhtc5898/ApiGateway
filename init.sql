0bd66d867db9f939e7fad744e5ff66e021077e85
jtenesaca <jtenesaca@ec.krugercorp.com>
2024-07-08 14:24:17 -0500

    Manejo de jaeger para ver peticiones

--- init.sql
+++ init.sql
@@ -9,6 +9,10 @@ CREATE TABLE servicio (
 
 
 INSERT INTO servicio (id,url,nombre,estado) VALUES
-	 ('servicio1','http://192.168.88.5:8090','Servicio 1','1'),
-	 ('servicio2','http://192.168.88.5:8090','Servicio 2','1'),
-	 ('servicio3','http://192.168.88.5:8090','Servicio 3','1');
+	 ('servicio1','http://192.168.88.7:8090','Servicio 1','1'),
+	 ('servicio2','http://172.16.26.27:5002','Servicio 2','1'),
+	 ('servicio3','http://172.16.26.27:3000','Servicio 3','1'),
+     ('servicio4','http://172.16.26.27:5001','Servicio 4','1'),
+	 ('servicio5','http://34.88.154.99','Servicio 5','1');
+
+
